d3c4bcb8af682f473b75f96cb8e237af0a3b624b
PushkarRJain <prj20012016@gmail.com>
2021-03-08 07:52:26 +0530

    Seetings+Signout Backend

--- index.js
+++ index.js
@@ -11,19 +11,34 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({
   extended: true
 }));
-
+global.username="";
+global.budget_val=0;
+global.flag=0;
 mongoose.connect("mongodb+srv://pj-admin:test123@cluster0.tclkd.mongodb.net/userDB", {useNewUrlParser: true});
 const userSchema = new mongoose.Schema ({
   email: String,
-  password: String
+  password: String,
+  budget: Number
 });
 
+const expenseSchema = new mongoose.Schema ({
+  email: String,
+  category: String,
+  cost: Number,
+  item: String,
+  date:
+  {
+    type:Date, 
+    default:Date.now()
+  },
+  delete:Boolean
+});
 const secret = "Thisisourlittlesecret.";
 userSchema.plugin(encrypt, { secret: secret, encryptedFields: ["password"] });
 
 
 const User = new mongoose.model("User", userSchema);
-
+const Expense = new mongoose.model("Expense",expenseSchema);
 app.get("/", function(req, res){
   res.sendFile(__dirname+'/index.html');
 });
@@ -34,15 +49,29 @@ app.get("/register", function(req, res){
   res.sendFile(__dirname+'/register.html');
 });
 app.get("/home", function(req, res){
-  res.sendFile(__dirname+'/home.html');
+  if(flag==1){res.sendFile(__dirname+'/home.html');}
+  else{res.redirect('/')}
 });
 app.get("/settings", function(req, res){
-  res.sendFile(__dirname+'/settings.html');
+  if(flag==1){res.sendFile(__dirname+'/settings.html');}
+  else{res.redirect('/')}
 });
 app.get("/dashboard", function(req, res){
-  res.sendFile(__dirname+'/dashboard.html');
+  if(flag==1){res.sendFile(__dirname+'/dashboard.html');}
+  else{res.redirect('/')}
+});
+app.get("/signout", function(req, res){
+  if(flag==1)
+  {
+    flag=0;
+    console.log(flag);
+    res.redirect('/');
+    
+  }
+  else{res.redirect('/')}
 });
 app.post("/register", function(req, res){
+  username = req.body.username;
   const newUser =  new User({
     email: req.body.username,
     password: req.body.password
@@ -51,13 +80,14 @@ app.post("/register", function(req, res){
     if (err) {
       console.log(err);
     } else {
+      flag=1;
       res.sendFile(__dirname+'/home.html');
     }
   });
 
 });
 app.post("/login", function(req, res){
-  const username = req.body.username;
+  username = req.body.username;
   const password = req.body.password;
   console.log(username);
   console.log(password);
@@ -67,6 +97,7 @@ app.post("/login", function(req, res){
     } else {
       if (foundUser) {
         if (foundUser.password === password) {
+          flag=1;
           res.sendFile(__dirname+'/home.html');
         }
       }
@@ -75,7 +106,16 @@ app.post("/login", function(req, res){
 });
 app.post("/budget",function(req,res){
   var budget= Number(req.body.budget);
+  budget_val = budget;
   console.log(budget)
+  User.updateOne({email:username},{budget:budget},function(err){
+    if(err){
+         console.log(err);
+       }
+    else{
+         console.log('All good!')
+       }
+     })
   res.sendFile(__dirname+'/settings.html');
 })
 app.post("/expenditure",function(req,res){
@@ -85,7 +125,30 @@ app.post("/expenditure",function(req,res){
   console.log(category);
   console.log(item);
   console.log(cost);
-  res.sendFile(__dirname+'/settings.html');
+  const newExpense =  new Expense({
+    email: username,
+    category: category,
+    item: item,
+    cost: cost,
+    delete:0
+  });
+  newExpense.save(function(err){
+    if (err) {
+      console.log(err);
+    } else {
+      budget_val = budget_val-cost;
+      User.updateOne({email:username},{budget:budget_val},function(err){
+        if(err){
+             console.log(err);
+           }
+        else{
+             console.log('All good!')
+           }
+         })
+      res.sendFile(__dirname+'/settings.html');
+    }
+  });
+  
 })
 app.listen(3000, function() {
   console.log("Server started on port 3000.");
